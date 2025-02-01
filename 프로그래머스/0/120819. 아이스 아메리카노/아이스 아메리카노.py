985d6efe313456b484575760fe0dc2c17c826da0
jinju <103505641+jinju0323@users.noreply.github.com>
2025-02-02 02:28:56 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,5 +1,7 @@
 def solution(money):
-    price = 5500
-    americano = money // price
-    m = money % price
-    return [americano, m]
+    # price = 5500
+    # americano = money // price
+    # m = money % price
+    # return [americano, m]
+    
+    return [money // 5500, money % 5500]
