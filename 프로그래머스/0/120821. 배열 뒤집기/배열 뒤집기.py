04c45394f1e9f6e90e664096a42e1cb6ce93f0b9
jinju <103505641+jinju0323@users.noreply.github.com>
2025-02-02 00:17:01 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,6 +1,15 @@
 def solution(num_list):
+    # 파이썬 기능
     # num_list.reverse()
     # return num_list
     
     # 리스트 슬라이싱
-    return num_list[::-1]
+    # return num_list[::-1]
+    
+    # 투 포인터
+    n = len(num_list)
+    for i in range(n//2):
+        p = n - i - 1
+        num_list[i], num_list[p] = num_list[p], num_list[i]
+        
+    return num_list 
