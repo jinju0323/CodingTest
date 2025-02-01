77829550a20e1af05f2f6550d663a3ced653d0a9
jinju <103505641+jinju0323@users.noreply.github.com>
2025-02-02 02:54:16 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,8 @@
 def solution(my_string):
-    vowels = 'aeiou'
-    answer = ''
-    for char in my_string:
-        if char not in vowels:
-            answer += char
-    return answer
+    # vowels = 'aeiou'
+    # answer = ''
+    # for char in my_string:
+        # if char not in vowels:
+            # answer += char
+    # return answer
+    return "".join([char for char in my_string if char not in "aeiou"])
