3734344d9a52e2d340d8fddcf864fdb6947e757e
jinju <103505641+jinju0323@users.noreply.github.com>
2025-02-02 03:02:43 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,2 +1,3 @@
 def solution(my_string, letter):
-    return "".join([char for char in my_string if char not in letter])
+    # return "".join([char for char in my_string if char not in letter])
+    return my_string.replace(letter, '')
