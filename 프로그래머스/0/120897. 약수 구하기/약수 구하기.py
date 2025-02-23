5536f048325e2a72983bc3565f4167d52a1aed67
jinju <103505641+jinju0323@users.noreply.github.com>
2025-02-23 23:52:34 +0900

    [level 0] Title: 약수 구하기, Time: 0.54 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,2 +1,8 @@
 def solution(n):
-    return [i for i in range(1, n + 1) if n % i == 0]
+    answer = []
+
+    for i in range(1, n + 1):
+        if n % i == 0:
+            answer.append(i)
+
+    return answer
